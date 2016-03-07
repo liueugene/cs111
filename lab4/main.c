6689bcf8bf3d46781e53d5ccacc8bee9b7cb3827
Eugene Liu <eugeneliu96@gmail.com>
2016-03-06 22:47:26 -0800

    compiles now

--- lab4/main.c
+++ lab4/main.c
@@ -7,6 +7,7 @@
 
 #define THREADS 1
 #define ITERATIONS 2
+#define OPT_YIELD 3
 
 void add(long long *pointer, long long value) {
     long long sum = *pointer + value;
@@ -15,103 +16,105 @@ void add(long long *pointer, long long value) {
 
 int no_of_threads = 1;
 int iterations = 1;
+int opt_yield = 0;
 
 long long counter = 0;
 
-void add_func(int* thread_no)
+void *add_func(int* thread_no)
 {
-    clock_t begin, end;
-    double time_spent;
-
-    int i;
 
 
-    begin = clock();
+    int i;    
 
-    int num_of_adds = iterations / no_of_threads;
-    if (*thread_no < (iterations % no_of_threads)) {
-        num_of_adds++;
-    }
 
-    for (i = 0; i < num_of_adds; i++) {
+    
+    for (i = 0; i < iterations; i++) {
         add(&counter, 1);
     }
 
-    for (i = 0; i < num_of_adds; i++) {
+    for (i = 0; i < iterations; i++) {
         add(&counter, -1);
     }
-
-    end = clock();
-
-    time_spent = (double) ((end - begin) / CLOCKS_PER_SEC);
-
-    pthread_exit(time_spect);
+    
+    pthread_exit(NULL);
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
     static struct option options[] = {
         {"threads", required_argument, 0, THREADS},
-        {"iterations", required_argument, 0, ITERATIONS},
+        {"iter", required_argument, 0, ITERATIONS},
+        {"opt_yield", required_argument, 0, OPT_YIELD},
         {0, 0, 0, 0}
     };
     
-    pthread_t* threads[no_of_threads];
-    double runtime;
-    double tot_runtime;
     int nsecs;
     int no_of_ops;
-
     int opt;
+    int i;
     
     while ((opt = getopt_long(argc, argv, "", options, NULL)) != -1) {
     
         char *endptr;
         switch (opt) {
             case THREADS:
-                no_of_threads = strtol(argv[optind], &endptr, 10);
-                if (endptr == argv[optind]) {
+                no_of_threads = strtol(argv[optind - 1], &endptr, 10);
+                if (endptr == argv[optind] || no_of_threads < 1) {
                     fprintf(stderr, "Invalid number of threads\n");
                     exit(1);
                 }
                 break;
             case ITERATIONS:
-                iterations = strtol(argv[optind], &endptr, 10);
-                if (endptr == argv[optind]) {
-                    fprintf(stderr, "Invalid number of threads\n");
+                iterations = strtol(argv[optind - 1], &endptr, 10);
+                if (endptr == argv[optind] || iterations < 1) {
+                    fprintf(stderr, "Invalid number of iterations\n");
                     exit(1);
                 }
                 break;
+            case OPT_YIELD:
+                opt_yield = strtol(argv[optind - 1], &endptr, 10);
+                if (endptr == argv[optind] || (opt_yield != 0 && opt_yield != 1)) {
+                    fprintf(stderr, "Invalid number for opt_yield\n");
+                    exit(1);
+                }
+            default:
+            {
+                fprintf(stderr, "Invalid argument\n"); 
+                exit(1); 
+            }
         }
     }
 
+    pthread_t threads[no_of_threads];
+    struct timespec begin, end;
+    
+    clock_gettime(CLOCK_REALTIME, &begin);
+    
     //create and run threads
     for (i = 0; i < no_of_threads; i++) {
-        if (pthread_create(&threads[i], NULL, &add_func, &i)) {
+        if (pthread_create(&threads[i], NULL, (void* (*)(void *))add_func, &i)) {
             fprintf(stderr, "Error creating thread %d.\n", i);
             return 1;
         }
     }
 
-    tot_runtime = 0;
     //wait for threads
     for (i = 0; i < no_of_threads; i++) {
-        if (pthread_join(threads[i], &runtime)) {
+        if (pthread_join(threads[i], NULL)) {
             fprintf(stderr, "Error waiting for thread %d.\n", i);
             return 1;
         }
-        tot_runtime += runtime;
     }
-
-    nsecs = tot_runtime * 1000000000;
-    no_of_ops = threads * iterations * 2;
     
-    printf("%d threads * %d iterations x (add + subtract) = %d operations\n", no_of_threads, iterations, num_of_ops);
-    if (counter == 0) {
-        printf("final count = 0\n");
-    }
-    else {
-        printf("ERROR: final count = %d\n", counter);
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    nsecs = ((end.tv_sec - begin.tv_sec) * 1000000000) + (end.tv_nsec - begin.tv_nsec);
+    no_of_ops = no_of_threads * iterations * 2;
+    
+    printf("%d threads * %d iterations x (add + subtract) = %d operations\n", no_of_threads, iterations, no_of_ops);
+    if (counter != 0) {
+        fprintf(stderr, "ERROR: final count = %lld\n", counter);
     }
     printf("elapsed time: %d ns\n", nsecs);
     printf("per operation: %d ns\n", nsecs / no_of_ops);
